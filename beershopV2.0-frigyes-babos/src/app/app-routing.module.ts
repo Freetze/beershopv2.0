930d3cc148b4d617ec14c9a7863783c34661ead5
Freetze <babos1221@gmail.com>
2022-10-21 15:16:18 +0200

    new components, routing, header

--- beershopV2.0-frigyes-babos/src/app/app-routing.module.ts
+++ beershopV2.0-frigyes-babos/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'details', component: DetailsPageComponent },
+  { path: 'shoppingcart', component: ShoppingCartPageComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- beershopV2.0-frigyes-babos/src/app/app.module.ts
+++ beershopV2.0-frigyes-babos/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
+import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
+import { BeerService } from './services/beer.service';
+import { HeaderComponent } from './components/header/header.component';
+import { LanguageSelectComponent } from './components/language-select/language-select.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DetailsPageComponent,
+    ShoppingCartPageComponent,
+    HeaderComponent,
+    LanguageSelectComponent,
+    SearchBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [BeerService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
